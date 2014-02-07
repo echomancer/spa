e1ed1a4668dca551c7ff4bfe91c8df523ab55c7f
Jason Perkins <jasontperkins@gmail.com>
2014-02-07 16:03:27 -0500

    Merging in work with old fss version

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,4 +13,14 @@ class ApplicationController < ActionController::Base
 		redirect_to root_path, :alert => exception.message
 	end
 
+	protected
+
+		def configure_permitted_parameters
+			devise_parameter_sanitizer.for(:sign_up) do |u|
+				u.permit(:name, :email, :password, :password_confirmation, :current_password, :role_ids)
+			end
+			devise_parameter_sanitizer.for(:account_update) do |u|
+				u.permit(:name, :email, :password, :password_confirmation, :current_password, :role_ids)
+			end
+		end
 end
